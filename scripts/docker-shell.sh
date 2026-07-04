7bf7277b7b4ed473457635b6e17f510626c22a7f
Tomer Laor <tomer.laor1@gmail.com>
2026-07-05 00:15:56 +0300

    Fix Docker OP-TEE end-to-end build

--- scripts/docker-shell.sh
+++ scripts/docker-shell.sh
@@ -6,8 +6,9 @@ IMAGE_NAME="${IMAGE_NAME:-confidential-computing-optee:latest}"
 
 exec docker run --rm -it \
   --platform linux/amd64 \
+  --user "$(id -u):$(id -g)" \
+  -e HOME=/tmp \
   -v "$ROOT_DIR:/workspace/ConfidentialComputing" \
   -w /workspace/ConfidentialComputing \
   "$IMAGE_NAME" \
   /bin/bash
-
